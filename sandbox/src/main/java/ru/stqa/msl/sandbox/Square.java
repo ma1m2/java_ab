239981a111f60db9078aa5d67a457606378016b1
ma1m2 <ma1m2@mail.ru>
2017-09-17 16:14:11 +0300

    Вычисляем площади

---
+++ sandbox/src/main/java/ru/stqa/msl/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.msl.sandbox;
+
+public class Square {
+  double a;
+
+  public Square(double a) {
+    this.a = a;
+  }
+
+  public double area(){
+    return this.a * this.a;
+  }
+}
