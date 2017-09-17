78c82896d055cb49a7d7a588da1a6f1a896572c8
ma1m2 <ma1m2@mail.ru>
2017-09-17 07:50:12 +0300

    Изменен пакет

--- sandbox/src/main/java/ru/stqa/msl/sandbox/MyFirstPr.java
+++ sandbox/src/main/java/ru/stqa/msl/sandbox/MyFirstPr.java
@@ -1,9 +1,9 @@
 package ru.stqa.msl.sandbox;
 
 public class MyFirstPr {
-	
-	public static void main(String[] args){
-		System.out.println("Hello, world");
-	}
-	
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+  }
+
 }
