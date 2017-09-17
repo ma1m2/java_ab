aa612ac8d0671eb1f76f40bb16f101b373eec8ff
ma1m2 <ma1m2@mail.ru>
2017-09-17 17:13:34 +0300

    Метод для вычисления расстояния между двумя точками перенесен в класс Point

--- sandbox/src/main/java/ru/msl/task2/Launcher.java
+++ sandbox/src/main/java/ru/msl/task2/Launcher.java
@@ -2,12 +2,12 @@ package ru.msl.task2;
 
 public class Launcher {
   public static void main(String[] args) {
-    Point p1 = new Point(2,1);
+    Point p1 = new Point(1,1);
     Point p2 = new Point(5,6);
-    System.out.printf("Distance between two points is %.2f \n",distance(p1,p2));
+    System.out.printf("Distance between two points is %.2f \n", p1.distance(p1,p2));
   }
 
-  private static double distance(Point p1, Point p2){
+/*  private static double distance(Point p1, Point p2){
     return Math.sqrt(Math.pow((p1.x - p2.x),2) + Math.pow((p1.y - p2.y),2));
-  }
+  }*/
 }
