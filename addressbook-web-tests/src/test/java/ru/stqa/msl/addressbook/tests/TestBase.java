97a64eecb8d9a6683650939c83d2d7f0914144d9
ma1m2 <ma1m2@mail.ru>
2017-09-30 19:57:38 +0300

    3.3 Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/msl/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/msl/addressbook/tests/TestBase.java
@@ -1,12 +1,16 @@
 package ru.stqa.msl.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.msl.addressbook.appmanager.ApplicationManager;
 
+
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  //protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+  //protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod
   public void setUp() throws Exception {
