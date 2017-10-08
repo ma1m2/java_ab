5b7847b1f3fa5d894aead355e39c6b75f4b0111f
ma1m2 <ma1m2@mail.ru>
2017-10-08 21:13:36 +0300

    Lesson 4.9 Лямбда-выражения

--- addressbook-web-tests/src/test/java/ru/stqa/msl/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/msl/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.msl.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase{
@@ -12,17 +13,30 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test3", "test1", "test1");
+    GroupData group = new GroupData("test4", "test1", "test1");
     app.getGroupHelper().creatGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
 
-    int max = 0;
+    /*int max = 0;
     for (GroupData gD : after){
       if (gD.getId() > max) {
         max = gD.getId();
       }
-    }
-    group.setId(max);
+    }*/
+
+    /*Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+      @Override
+      public int compare(GroupData o1, GroupData o2) {
+        return Integer.compare(o1.getId(), o2.getId());
+      }
+    };
+    int max1 = after.stream().max(byId).get().getId();*/
+
+    /*Comparator<? super GroupData> byId = (Comparator<GroupData>)
+            (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+    int max1 = after.stream().max(byId).get().getId();*/
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
 
     Assert.assertEquals(after, before);
