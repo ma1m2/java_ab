c213e9c441bda3dac74fa36fa38b35a5c4173fd3
ma1m2 <ma1m2@mail.ru>
2017-10-20 06:14:40 +0300

    HW#9 ContactDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/msl/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/msl/addressbook/tests/ContactDeletionTests.java
@@ -1,11 +1,13 @@
 package ru.stqa.msl.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.Date;
 import java.io.File;
@@ -27,9 +29,14 @@ public class ContactDeletionTests extends TestBase {
       app.getContactHelper().creatContact(new ContactData(1,"Ru", "Ruru",
               "+73339993118", "test113@mail.ru", "test1"), true);
     }
-    app.getContactHelper().selectContact(1);
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().deleteSelectedContact();
     app.getContactHelper().closeAlert();
+    app.getNavigationHelper().goToHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before,after);
   }
 
 }
