da648ea6b8c55f5fccaf9f73c437c274bbb189a6
ma1m2 <ma1m2@mail.ru>
2017-11-11 20:23:49 +0300

    7.4 Сравнение данных из БД

--- addressbook-web-tests/src/test/java/ru/stqa/msl/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/msl/addressbook/tests/GroupModificationTests.java
@@ -14,9 +14,25 @@ public class GroupModificationTests extends TestBase {
   @BeforeMethod
   public void ensurePrecondition(){//обеспечить предварительное условие
     app.goTo().groupPage();
-    if (app.group().count()==0){
+    if (app.db().groups().size()==0){
       app.group().create(new GroupData().withName("test1"));
     }
+/*    app.goTo().groupPage();
+    if (app.group().count()==0){
+      app.group().create(new GroupData().withName("test1"));
+    }*/
+  }
+
+  @Test
+  public void testGroupModificationDb(){
+    Groups before = app.db().groups();
+    GroupData modifiedGroup = before.iterator().next();
+    GroupData group = new GroupData().withId(modifiedGroup.getId())
+            .withName("test10").withHeader("test7").withFooter("test2");
+    app.group().modify(group);
+    assertEquals(app.group().count(),before.size());
+    Groups after = app.db().groups();
+    assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 
   @Test(enabled = false)
